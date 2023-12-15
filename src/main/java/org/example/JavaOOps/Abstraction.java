f53e7ff1685f07acd0ad42b2fb79ef72c53b6c64
DevisettyBhargavaKS <bhargava.devisetty@kumaran.com>
2023-12-15 10:47:29 +0530

    Initial commit

---
+++ src/main/java/org/example/JavaOOps/Abstraction.java
@@ -0,0 +1,26 @@
+package org.example.JavaOOps;
+abstract class animal{
+    private String name;
+
+    public void Animal(String name) { this.name = name; }
+
+
+
+    public String getName() { return name; }
+}
+ class dog extends animal {
+
+
+
+     public void makeSound() {
+         System.out.println("barking");
+
+     }
+ }
+public class Abstraction {
+    public static void main(String[] args)
+    {
+        dog d=new dog();
+        d.makeSound();
+    }
+}
